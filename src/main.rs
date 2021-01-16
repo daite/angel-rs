67a7d8e4820f9399337958afbfa0874bd66e378f
daite <blueskykind02@yahoo.co.jp>
2021-01-16 09:19:53 +0900

    updated

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ fn main() {
         .version("0.1.0")
         .author("daite <blueskykind02@yahoo.co.jp>")
         .about("search torrent files")
-        .arg(Arg::with_name("search")
+        .arg(Arg::with_name("keyword")
                  .short("s")
                  .long("search")
                  .takes_value(true)
                  .help("search torrent magnet file"))
         .get_matches();
-    let myfile = matches.value_of("search").unwrap_or("동상이몽");
+    let myfile = matches.value_of("keyword").unwrap_or("동상이몽");
    if let Err(e) = ttobogo::run(myfile) {
        println!("{}", e);
    }
